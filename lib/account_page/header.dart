6e13e95ec4d044cb49eb6dbf833172dc7049b7d5
Rizki R <idayirikzir@gmail.com>
2022-01-26 10:26:41 +0700

    add account page

---
+++ lib/account_page/header.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:paylater_startup/util/balance_information.dart';
+import 'package:paylater_startup/util/colors.dart';
+import '../util/size_config.dart' as size_conf;
+
+Widget Header(double _width, double _height) {
+
+  return Stack(
+    alignment: Alignment.topCenter,
+    children: [
+
+      AspectRatio(
+        aspectRatio: size_conf.screenSizeIndex(_width)>2?size_conf.screenSizeIndex(_width)>5?16/4:16/4:4/3,
+        child: Container(
+          width: _width,
+          height: _height,
+          decoration: const BoxDecoration(
+            color: kPrimaryColor,
+            borderRadius: BorderRadius.only(bottomLeft: Radius.circular(50), bottomRight: Radius.circular(50)),
+          ),
+        ),
+      ),
+
+      Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: _height * 0.1),
+
+          Row(
+            children: [
+              const Text("NAMA", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30, fontFamily: 'Gilroy')),
+              SizedBox(width: _width * 0.35,),
+              Container(
+                width: _width * 0.2,
+                child: const Text("NAMA", style: TextStyle(fontWeight: FontWeight.normal, fontSize: 15, fontFamily: 'Gilroy')),
+                decoration: const BoxDecoration(
+                  color: scaffoldColor,
+                  borderRadius: BorderRadius.only(bottomLeft: Radius.circular(50), bottomRight: Radius.circular(50)),
+                )
+              )
+            ],
+          ),
+
+          const Text("Verified", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30, fontFamily: 'Gilroy')),
+          Row(
+            children:const [
+              Text("+62819181981", style: TextStyle(fontSize: 14, fontFamily: 'Gilroy')),
+              Text("|", style: TextStyle(fontSize: 14, fontFamily: 'Gilroy')),
+              Text("email@email.com", style: TextStyle(fontSize: 14, fontFamily: 'Gilroy')),
+            ]
+          ),
+          Row(
+            children: const [
+              Icon(Icons.place),
+              Text("Address", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14, fontFamily: 'Gilroy')),
+            ],
+          ),
+
+          SizedBox(height: _height * 0.2),
+
+          balanceInformation(_width, _height),
+        ],
+      ),
+    ],
+  );
+}
---
+++ lib/account_page/home.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:paylater_startup/home_page/bottom_bar.dart';
+
+import 'header.dart';
+
+class AccountPage extends StatelessWidget {
+
+  double width;
+  double height;
+
+  AccountPage(this.width, this.height, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SizedBox(
+          width: width,
+          height: height,
+          child: SingleChildScrollView(
+            child: Wrap(
+              children: [
+                Header(width, height),
+                HomeBody(width, height),
+              ],
+            ),
+          ),
+        ),
+      ),
+      bottomNavigationBar: const bottomNavigationBar(),
+    );
+  }
+}
